81806859989de3d378671f272f9989d8cb5b5bf9
AndrejRu <andrej.russkov@stud.hs-mannheim.de>
2016-06-08 18:03:24 +0200

    Filter working

--- Meilenstein 3/server.js
+++ Meilenstein 3/server.js
@@ -1,27 +1,31 @@
-var express = require('express');
-var app = express();
-var fs=require('fs');
-
-app.get('/home', function (req, res) {
-    res.send('Hello Home!');
-});
+const express = require('express');
+const app = express();
+const fs=require('fs');
 
 app.get('/api/players',(req,res) =>{
     fs.readFile('./players.json','utf-8',(err,data)=>{
-    console.log(data);
-res.send(data);
+    console.log('Got all Players');
+    res.send(data);
 })
 })
 
 app.get('/api/players/fav',(req,res) =>{
-   var json= JSON.parse(fs.readFileSync('./players.json','utf8'));
-    json.
-    res.send(json);
+    var json= JSON.parse(fs.readFileSync('./players.json','utf8'));
+    var array= [];
+    for(var x in json){
+        array.push(json[x]);
+    }
+    var arrayfil=array.filter(function(el){
+        return (el.favorit===true);
+    })
+    console.log(arrayfil)
+    res.send(array);
 })
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+function checkFav(value){
+    return 'favorit: true';
+}
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
